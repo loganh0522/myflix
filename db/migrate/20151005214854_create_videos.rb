99d595db058269fb7febdf8050abf4b430f4ec91
loganh0522 <loganh0522@gmail.com>
2015-10-06 20:23:05 -0400

    Added Video Migration/Model & Seeded Data

--- db/migrate/20151005214854_create_videos.rb
+++ db/migrate/20151005214854_create_videos.rb
@@ -1,7 +1,7 @@
 class CreateVideos < ActiveRecord::Migration
   def change
     create_table :videos do |t|
-    	t.string :title
+    	t.string :title, :small_cover_url, :large_cover_url
     	t.text :description
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Video.create(title: "Star Wars", description: "Awesome")
+Video.create(title: "Lord of the Rings", description: "Cool")
